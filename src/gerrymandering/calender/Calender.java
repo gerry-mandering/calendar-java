bea8991ae5ddd5e7fbaa783474f544e49dceffa7
gerry-mandering <kimmen1119@gmail.com>
2022-04-26 15:36:47 +0900

    Feat: 반복입력 기능 추가

--- src/gerrymandering/calender/Calender.java
+++ src/gerrymandering/calender/Calender.java
@@ -6,11 +6,29 @@ public class Calender {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         DaysPrinter daysPrinter = new DaysPrinter();
+        int month = 0;
+
+        while (true) {
+            System.out.print("달을 입력하시오 (종료는 -1) : ");
+            month = sc.nextInt();
+            if (month == -1) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println(month + "월은 " + daysPrinter.printDays(month) +"일까지 있습니다.");
+            }
+        }
+
+        sc.close();
+
+/*        Scanner sc = new Scanner(System.in);
+        DaysPrinter daysPrinter = new DaysPrinter();
 
         System.out.print("달을 입력하시오 : ");
         int month = sc.nextInt();
 
         System.out.println(month + "월은 " + daysPrinter.printDays(month) + "일까지 있습니다.");
+        sc.close();*/
 
 /*        Scanner sc = new Scanner(System.in);
         Adder adder = new Adder();
