04035f028dc352dd734da7ca90330574e9841c58
gerry-mandering <kimmen1119@gmail.com>
2022-04-26 22:28:53 +0900

    Feat: Zeller's congruence를 이용한 요일탐색기 추가, 달력출력기 완성

--- src/gerrymandering/calender/Calender.java
+++ src/gerrymandering/calender/Calender.java
@@ -6,7 +6,6 @@ public class Calender {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CalenderPrinter calenderPrinter = new CalenderPrinter();
-//        DaysPrinter daysPrinter = new DaysPrinter();
         int month, year = 0;
 
         while (true) {
@@ -14,33 +13,14 @@ public class Calender {
             System.out.print("년도를 입력하시오 : ");
             year = sc.nextInt();
             if (detectMinusOne(year)) break;
+
             System.out.print("달을 입력하시오 : ");
             month = sc.nextInt();
             if (detectMinusOne(month)) break;
+
             calenderPrinter.printCalender(year, month);
-//                System.out.println(month + "월은 " + daysPrinter.printDays(month) +"일까지 있습니다.");
         }
-
         sc.close();
-
-/*        Scanner sc = new Scanner(System.in);
-        DaysPrinter daysPrinter = new DaysPrinter();
-
-        System.out.print("달을 입력하시오 : ");
-        int month = sc.nextInt();
-
-        System.out.println(month + "월은 " + daysPrinter.printDays(month) + "일까지 있습니다.");
-        sc.close();*/
-
-/*        Scanner sc = new Scanner(System.in);
-        Adder adder = new Adder();
-
-        System.out.print("두 수를 입력하시오 : ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println("두 수의 합은 " + adder.sum(a, b) + "입니다.");
-        sc.close();*/
     }
 
     private static boolean detectMinusOne(int year) {
--- src/gerrymandering/calender/CalenderPrinter.java
+++ src/gerrymandering/calender/CalenderPrinter.java
@@ -1,38 +1,40 @@
 package gerrymandering.calender;
 
+import static java.lang.Math.*;
+
 public class CalenderPrinter {
-    private final String day31 = ("일  월  화  수  목  금  토\n" +
-            "--------------------\n" +
-            " 1  2  3  4  5  6  7\n" +
-            " 8  9 10 11 12 13 14\n" +
-            "15 16 17 18 19 20 21\n" +
-            "22 23 24 25 26 27 28\n" +
-            "29 30 31");
+    // Zeller's congruence를 이용한 요일 찾기
+    static int getDay(int year, int month) {
+        int h = 0;                  // 요일(0~6 == 토~일)
+        int q = 1;                  // 일
+        double m = month;           // 월
+        double K = (int)year % 100; // 연도 마지막 두 자리 (2015 -> 15)
+        double J = (int)year / 100; // 세기
 
-    private final String day30 = ("일 월 화 수 목 금 토\n" +
-            "--------------------\n" +
-            " 1  2  3  4  5  6  7\n" +
-            " 8  9 10 11 12 13 14\n" +
-            "15 16 17 18 19 20 21\n" +
-            "22 23 24 25 26 27 28\n" +
-            "29 30");
+        if (m <= 2) {                   // 1월과 2월은 m에 12를 더해줌
+            m += 12;
+            K = (int)(year - 1) % 100;  // 연도는 1을 빼준값을 이용
+            J = (int)(year - 1) / 100;
+        }
 
-    private final String day28 = ("일 월 화 수 목 금 토\n" +
-            "--------------------\n" +
-            " 1  2  3  4  5  6  7\n" +
-            " 8  9 10 11 12 13 14\n" +
-            "15 16 17 18 19 20 21\n" +
-            "22 23 24 25 26 27 28");
+        // Zeller's congruence
+        h = (q +
+                (int)floor((13 * (m + 1)) / 5) +
+                (int)K +
+                (int)floor(K / 4) +
+                (int)floor(J / 4) -
+                (int)(2 * J)
+            ) % 7;
 
-    private final String day29 = ("일 월 화 수 목 금 토\n" +
-            "--------------------\n" +
-            " 1  2  3  4  5  6  7\n" +
-            " 8  9 10 11 12 13 14\n" +
-            "15 16 17 18 19 20 21\n" +
-            "22 23 24 25 26 27 28\n" +
-            "29");
+        if (h < 0) {    // 나머지 값 결과가 음수일 경우 값을 보정
+            h += 7;
+        }
 
-    static boolean classifyLeapYear(int year) {
+        System.out.println("h = " + h);
+        return h;
+    }
+
+    static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             if (year % 400 != 0 && year % 100 == 0) {
                 return false;
@@ -44,24 +46,51 @@ public class CalenderPrinter {
         }
     }
 
-    void printCalender(int year, int month) {
-        if (classifyLeapYear(year) && month == 2) {
-            System.out.println(day29);
+    static int getEndDay(int year, int month) {
+        if (isLeapYear(year) && month == 2) {
+            return  29;
         } else {
             switch (month) {
                 case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                    System.out.println(day31);
-                    break;
+                    return 31;
                 case 4: case 6: case 9: case 11:
-                    System.out.println(day30);
-                    break;
+                    return  30;
                 case 2:
-                    System.out.println(day28);
-                    break;
+                    return  28;
                 default:
                     System.out.println("달을 제대로 입력하시오!");
                     System.exit(0);
+                    return 0;
             }
+        }
+    }
+
+    void printCalender(int year, int month) {
+        int dayOfTheWeek = getDay(year, month);
+        int date = 1, skipper = 0, endDay = getEndDay(year, month);
+
+        if (dayOfTheWeek == 0) {
+            skipper = 6;
+        } else {
+            skipper = dayOfTheWeek - 1;
+        }
+
+        System.out.printf("일\t월\t화\t수\t목\t금\t토\n" +
+                "--------------------------\n");
+
+        for (int i=0; i<6; i++) {
+            for (int j=0; j<7; j++) {
+                if (skipper > 0) {
+                    System.out.printf("\t");
+                    skipper -= 1;
+                } else if (date > endDay) {
+                    System.out.printf("\t");
+                } else {
+                    System.out.printf("%d\t", date);
+                    date++;
+                }
+            }
+            System.out.println();
         }
     }
 }
